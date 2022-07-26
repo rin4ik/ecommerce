1d74dd96ad1254839160a627971aeabd36df446f
Mirakhmad <m.mirakhmad@gmail.com>
2022-07-26 12:42:53 +0500

    eager load relations

--- app/Http/Controllers/ProductShowController.php
+++ app/Http/Controllers/ProductShowController.php
@@ -8,6 +8,7 @@ class ProductShowController extends Controller
 {
     public function __invoke(Product $product)
     {
+        $product->load('variations.children', 'variations.descendantsAndSelf.stocks');
         return view('products.show', [
             'product' => $product
         ]);

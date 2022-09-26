ae00f2339e9127730752e1f3967784aa51ce50c9
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-09-26 12:20:23 +0500

    add media converstions for variations

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
-class Variation extends Model
+class Variation extends Model implements HasMedia
 {
-    use HasFactory, HasRecursiveRelationships;
+    use HasFactory, HasRecursiveRelationships, InteractsWithMedia;
 
     public function product()
     {
@@ -39,5 +43,15 @@ class Variation extends Model
     public function formattedPrice()
     {
         return money($this->price);
+    }
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
+    }
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('default')
+            ->useFallbackUrl(url('/storage/no-product-image.png'));
     }
 }
